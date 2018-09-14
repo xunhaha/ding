c2d4c172888c167369ae544f34fa44ecd6cc89b7
xunziheng <xunziheng88@163.com>
2018-09-14 10:29:29 +0800

    路由

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,12 +1,12 @@
 <template>
-  <div id="app">
+  <div class="main">
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'index'
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import message from '@/components/message'
+import index from '@/components/index'
 
 Vue.use(Router)
 
@@ -8,8 +9,11 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'message',
-      component: message
+      name: 'index',
+      children:[
+        
+      ],
+      component: index
     }
   ]
 })
